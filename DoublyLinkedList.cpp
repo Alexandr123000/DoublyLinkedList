8e37e3d2e8770473a3f5aa9fe6e695a8789381e7
Alexandr123000 <alexandrt10000@gmail.com>
2026-04-15 19:51:59 +0300

    Добавьте файлы проекта.

---
+++ DoublyLinkedList.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+struct ListNode {
+	ListNode* prev = nullptr;
+	ListNode* next = nullptr;
+	ListNode* rand = nullptr;
+};
+
+int main()
+{
+	return 0;
+}
